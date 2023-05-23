b72cb85a5ebff996a14fcbb898cbfea647c3bb72
Tomato <Tomato229@gmail.com>
2023-05-24 00:06:06 +0900

    Turn in Place
    Have to fix clip transform

--- Source/ShooterGame/ShooterAnimInstance.cpp
+++ Source/ShooterGame/ShooterAnimInstance.cpp
@@ -4,6 +4,19 @@
 #include "ShooterAnimInstance.h"
 #include "Kismet/KismetMathLibrary.h"
 
+UShooterAnimInstance::UShooterAnimInstance() 
+{
+	Speed = 0.0f;
+	bIsInAir = false;
+	bIsAccelerating = false;
+	MovementOffsetYaw = 0.0f;
+	bAiming = false;
+	CharacterYaw = 0.0f;
+	LastMovementOffsetYaw = 0.0f;
+	CharacterYawLastFrame = 0.0f;
+	RootYawOffset = 0.0f;
+}
+
 void UShooterAnimInstance::NativeInitializeAnimation()
 {
 	ShooterCharacter = Cast<ACharacterBase>(TryGetPawnOwner());
@@ -48,5 +61,54 @@ void UShooterAnimInstance::NativeUpdateAnimation(float DeltaTime)
 		}
 
 		bAiming = ShooterCharacter->GetAiming();
+	}
+	TurnInPlace();
+}
+
+void UShooterAnimInstance::TurnInPlace()
+{
+	if (ShooterCharacter == nullptr) return;
+	if (Speed > 0)
+	{
+		RootYawOffset = 0.0f;
+		CharacterYaw = ShooterCharacter->GetActorRotation().Yaw;
+		CharacterYawLastFrame = CharacterYaw;
+		RotationCurveLastFrame = 0.0f;
+		RotationCurve = 0.0f;
+	}
+	else
+	{
+		CharacterYawLastFrame = CharacterYaw;
+		CharacterYaw = ShooterCharacter->GetActorRotation().Yaw;
+		const float YawDelta{ CharacterYaw - CharacterYawLastFrame };
+
+		RootYawOffset = UKismetMathLibrary::NormalizeAxis(RootYawOffset - YawDelta);
+
+		const float Turning{ GetCurveValue(TEXT("Turning")) };
+		if (Turning > 0)
+		{
+			RotationCurveLastFrame = RotationCurve;
+			RotationCurve = GetCurveValue(TEXT("Rotation"));
+
+			const float DeltaRotation{ RotationCurve - RotationCurveLastFrame };
+
+			// Turning Left
+			if (RootYawOffset > 0)
+			{
+				RootYawOffset -= DeltaRotation;
+			}
+			// Turning Right
+			else
+			{
+				RootYawOffset += DeltaRotation;
+			}
+
+			const float ABSRootYawOffset{ FMath::Abs(RootYawOffset) };
+			if (ABSRootYawOffset > 90.0f)
+			{
+				const float YawExcess{ ABSRootYawOffset - 90.0f };
+				RootYawOffset > 0 ? RootYawOffset -= YawExcess : RootYawOffset += YawExcess;
+			}
+		}
 	}
 }
--- Source/ShooterGame/ShooterAnimInstance.h
+++ Source/ShooterGame/ShooterAnimInstance.h
@@ -17,34 +17,57 @@ class SHOOTERGAME_API UShooterAnimInstance : public UAnimInstance
 	GENERATED_BODY()
 	
 public:
+	UShooterAnimInstance();
+
 	virtual void NativeInitializeAnimation() override;
 	virtual void NativeUpdateAnimation(float DeltaTime) override;
 
 public:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
 	ACharacterBase* ShooterCharacter;
 
-public:
+protected:
+	/** 제자리 회전 */
+	void TurnInPlace();
+
+private:
 	/** 캐릭터의 이동 속도 */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
 	float Speed;
 	
 	/** 캐릭터가 공중에 있는지 체크 */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
 	bool bIsInAir;
 	
 	/** 캐릭터가w 가속하는지 체크 */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
 	bool bIsAccelerating;
 
 	/** 캐릭터 이동 Yaw 오프셋 */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
 	float MovementOffsetYaw;
 
 	/** 정지하기 직전 Offset Yaw */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
 	float LastMovementOffsetYaw;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	/** 조준 여부 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
 	bool bAiming;
+
+	/** 현 프레임에서의 캐릭터 상반신 회전(Yaw) */
+	float CharacterYaw;
+
+	/** 이전 프레임에서의 캐릭터 상반신 회전(Yaw) */
+	float CharacterYawLastFrame;
+
+	/**  */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Turn In Place", meta = (AllowPrivateAccess = "true"))
+	float RootYawOffset;
+
+	/** 현 프레임의 Rotation Curve */
+	float RotationCurve;
+
+	/** 이전 프레임의 Rotation Curve */
+	float RotationCurveLastFrame;
 };
